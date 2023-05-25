2f9c4c0321c27da88f1c593080d6fdf588f3f029
Asifali Lal <132897903+asifalilal@users.noreply.github.com>
2023-05-25 12:28:21 +0530

    Add files via upload

---
+++ subscription plan.py
@@ -0,0 +1,16 @@
+
+
+
+month=0
+fees=1000
+
+umonth=int(input("Enter month of subscription: "))
+
+if(month != umonth):
+  print("your fees amount:  ",umonth*fees)
+else:
+   print("plzz select the right month")
+
+
+
+
