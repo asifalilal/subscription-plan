f2e2bcceb2dfc8dd36c81a3959bc3fc163316f15
rahemanlal <rahemanalilal.tech@gmail.com>
2023-06-20 17:57:12 +0530

    change umonth to user_month

--- subscription plan.py
+++ subscription plan.py
@@ -4,10 +4,10 @@
 month=0
 fees=5000
 
-umonth=int(input("Enter month of subscription: "))
+user_month=int(input("Enter month of subscription: "))
 
-if(month != umonth):
-  print("your fees amount:  ",umonth*fees)
+if(month != user_month):
+  print("your fees amount:  ",user_month*fees)
 else:
    print("plzz select the right month")
 
